6116499ca905194518c406c18e914bc86a532087
simonherlin <herlin.simon@gmail.com>
2018-11-13 18:36:02 +0100

    [ADD] method animate with animation

--- monte_carlo.py
+++ monte_carlo.py
@@ -20,4 +20,41 @@ axes1.plot(x, y, color='k', linestyle='-', linewidth=3)
 # Draw quarter circle
 circle1 = plt.Circle((0, 0), 1, color='k', fill=False, linewidth=3)
 axes1.add_artist(circle1)
+
+trials = 0
+inside_circle = 0
+x_outside, y_outside, x_inside, y_inside = [],[], [], []
+sc_outside = axes1.scatter(x_outside, y_outside)
+sc_outside.set_color('r')
+sc_inside = axes1.scatter(x_inside, y_inside)
+sc_inside.set_color('b')
+
+text1 = plt.text(1, 1.4, "trials = 0")
+text2 = plt.text(1, 1.3, "inside circle = 0")
+text3 = plt.text(1, 1.2, "value of PI = 0")
+
+# plot the points and update the pi value
+def animate(i):
+    global trials, inside_circle
+    trials = trials + 1
+    x = np.random.rand(1)
+    y = np.random.rand(1)
+
+    if x**2 + y**2 <= 1:
+        x_inside.append(x)
+        y_inside.append(y)
+        sc_inside.set_offsets(np.c_[x_inside, y_inside])
+        inside_circle = inside_circle + 1
+    else:
+        x_outside.append(x)
+        y_outside.append(y)
+        sc_outside.set_offsets(np.c_[x_outside, y_outside])
+
+    text1.set_text("trials = %d" % trials)
+    text2.set_text("inside circle = %d" % inside_circle)
+    pi = 4.0*inside_circle/trials
+    text3.set_text("pi = %f" % pi)
+
+# Call methode animate with animation for see the value of PI in the time
+ani = animation.FuncAnimation(figure, animate, frames=50, interval=1, repeat=True)
 plt.show()
